6f0f38a7276a905f256a2c113cd9e2267395fccf
seanylord <seanboyle2005@gmail.com>
2024-05-01 12:09:06 -0400

    Update bomb_configs.py

--- bomb_configs.py
+++ bomb_configs.py
@@ -9,7 +9,7 @@ DEBUG = True        # debug mode?
 RPi = True           # is this running on the RPi?
 ANIMATE = True       # animate the LCD text?
 SHOW_BUTTONS = False # show the Pause and Quit buttons on the main LCD GUI?
-COUNTDOWN = 300,      # the initial bomb countdown value (seconds)
+COUNTDOWN = 300      # the initial bomb countdown value (seconds)
 NUM_STRIKES = 500      # the total strikes allowed before the bomb "explodes"
 NUM_PHASES = 4       # the total number of initial active bomb phases
 
